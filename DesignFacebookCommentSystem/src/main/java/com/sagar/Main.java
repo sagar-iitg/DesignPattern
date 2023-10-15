1fa1df08016b2e9109b653bc01bc72dfaf9ddee6
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-10-15 12:02:21 +0530

    added Facebook Comment System

--- DesignFacebookCommentSystem/src/main/java/com/sagar/Main.java
+++ DesignFacebookCommentSystem/src/main/java/com/sagar/Main.java
@@ -1,7 +1,65 @@
 package com.sagar;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world1!");
+        User u1 = new User("Sagar");
+        User u2=new User("Mohit");
+        System.out.println(u1.getId());
+
+
+        Comment c1 = new Comment("First Comment");
+        Comment c2 = new Comment("Second Comment");
+        Comment c21 = new Comment("Second Nested Comment");
+        Comment c11 = new Comment("First Nested Comment");
+        Comment c12 = new Comment("First Second Nested Comment");
+        Comment c13 = new Comment("First Third Nested Comment");
+
+        Post p = new Post();
+
+        u1.addCommentToPost(p, c1);
+        u1.addCommentToPost(p, c2);
+
+        u1.replyToComment(p, c1.getId(), c11);
+        u1.replyToComment(p, c1.getId(), c12);
+        u1.replyToComment(p, c1.getId(), c13);
+        u1.replyToComment(p, c2.getId(), c21);
+
+        System.out.println("========================================================================");
+        System.out.println("Printing Comments on the post");
+        System.out.println("========================================================================");
+
+        for (Comment com : p.getComments()) {
+            System.out.println(com.getDescription());
+            for (Comment j : com.getComments()) {
+                System.out.println("\t" + j.getDescription());
+            }
+        }
+
+        System.out.println("========================================================================");
+        System.out.println("Editing Comments");
+        System.out.println("========================================================================");
+        u1.editComment(p, c1.getId(), c11.getId(), "Edited Comment");
+        u1.editComment(p, c2.getId(), c12.getId(), "Edited Comment 2");
+
+        System.out.println("========================================================================");
+        System.out.println("Deleting Comments");
+        System.out.println("========================================================================");
+        u1.deleteComment(p, c2.getId(), c12.getId());
+
+        System.out.println("========================================================================");
+        System.out.println("Printing Comments on the post");
+        System.out.println("========================================================================");
+
+        for (Comment com : p.getComments()) {
+            System.out.println(com.getDescription());
+            for (Comment j : com.getComments()) {
+                System.out.println("\t" + j.getDescription());
+            }
+        }
     }
 }
