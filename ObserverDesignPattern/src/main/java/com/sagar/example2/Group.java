811e7d29c0d6955df1baa6d7ea124e46fa90a11a
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-01-01 18:52:14 +0530

    Update Group.java

--- ObserverDesignPattern/src/main/java/com/sagar/example2/Group.java
+++ ObserverDesignPattern/src/main/java/com/sagar/example2/Group.java
@@ -3,6 +3,9 @@ package com.sagar.example2;
 import java.util.ArrayList;
 import java.util.List;
 
+
+//Subject Class
+//Publisher Class
 public class Group {
 
     List<Subscriber> users=new ArrayList<>();
