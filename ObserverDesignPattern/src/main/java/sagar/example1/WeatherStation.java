93b746ef7ec39911495e044a9f8f2c870bcc52e0
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-01-01 18:22:25 +0530

    Update WeatherStation.java

--- ObserverDesignPattern/src/main/java/sagar/example1/WeatherStation.java
+++ ObserverDesignPattern/src/main/java/sagar/example1/WeatherStation.java
@@ -1,10 +1,17 @@
 package sagar.example1;
 
+// Behavioral design patterns are concerned with algorithms
+// and the assignment of responsibilities between objects
+
 public class WeatherStation {
     public static void main(String[] args) {
 
         WeatherData weatherData=new WeatherData();
         CurrentConditionDisplay currentConditionDisplay=new CurrentConditionDisplay(weatherData);
+        WeatherData weatherData1=new WeatherData();
+        CurrentConditionDisplay currentConditionDisplay1=new CurrentConditionDisplay(weatherData1);
+        weatherData1.setMeasurement(100,90,30.2f);
+
         weatherData.setMeasurement(80,65,30.4f);
         weatherData.setMeasurement(82,70,29.2f);
         weatherData.setMeasurement(78,90,29.2f);
