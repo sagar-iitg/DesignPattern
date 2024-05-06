882b2d222859c8ff8c3d31309f60946fcd057438
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-05-06 23:50:30 +0530

    Create DBConnection.java

---
+++ SingletonDesignPattern/src/main/java/sagar/lazy/DBConnection.java
@@ -0,0 +1,26 @@
+package sagar.lazy;
+
+public class DBConnection {
+
+    private static DBConnection conObject;
+    private DBConnection(){
+
+    }
+
+    public static DBConnection getInstance(){
+        if(conObject==null) conObject=new DBConnection();
+        return conObject;
+    }
+
+}
+
+class Main{
+    public static void main(String[] args) {
+        DBConnection singletonObject=DBConnection.getInstance();
+        DBConnection singletonObject1=DBConnection.getInstance();
+        DBConnection singletonObject2=DBConnection.getInstance();
+        System.out.println(singletonObject.hashCode());
+        System.out.println(singletonObject1.hashCode());
+        System.out.println(singletonObject2.hashCode());
+    }
+}
