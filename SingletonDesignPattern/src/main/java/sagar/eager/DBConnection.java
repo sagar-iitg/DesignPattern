a39284768a9278d5bfb0d74ad6c786a4717c73f4
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-05-06 23:42:29 +0530

    Create DBConnection.java

---
+++ SingletonDesignPattern/src/main/java/sagar/eager/DBConnection.java
@@ -0,0 +1,23 @@
+package sagar.eager;
+
+public class DBConnection {
+
+    private static DBConnection conObject=new DBConnection();
+    private DBConnection(){
+
+    }
+
+    public static DBConnection getInstance(){
+        return conObject;
+    }
+
+}
+
+class Main{
+    public static void main(String[] args) {
+        DBConnection singletonObject=DBConnection.getInstance();
+        DBConnection singletonObject1=DBConnection.getInstance();
+        System.out.println(singletonObject.hashCode());
+        System.out.println(singletonObject1.hashCode());
+    }
+}
